69370391beaaccc9d15901238617158e00e4d690
DESKTOP-E4UEJB8\DELL <tazwar70@gmail.com>
2021-04-20 11:48:11 -0400

    overhaul-update

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -4,7 +4,7 @@ document.getElementById('footer').innerHTML =
     <li><a href="https://twitter.com/AranArnab"><i class="fab fa-twitter"></i></a></li>
     <li><a href="https://github.com/AurnabIslam?tab=repositories"><i class="fab fa-github"></i></a></li>
     <li><a href="https://www.instagram.com/_iaran_/"><i class="fab fa-instagram"></i></a></li>
-    <li><a href="https://www.deviantart.com/aurnabaran"><i class="fab fa-deviantart"></i></a></li>
+    <li><a href="https://www.linkedin.com/in/aurnab-i-0a915b132/"><i class="fab fa-linkedin"></i></a></li>
     
 </ul>
 <ul class="copyright">
